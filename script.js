dfc908ecc750a4883ad8d2e743f863e4e42d767e
unknown <makarand.gijam22@gmail.com>
2024-02-03 20:06:41 +0530

    improve ui of page

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
 // Getting Elements
-let opnBtn = document.querySelector('.open-dailog');
-let modal = document.querySelector('#modal');
-let closeBtn = document.querySelector('.close-dailog');
+const opnBtn = document.querySelector('.open-dailog');
+const modal = document.querySelector('#modal');
+const closeBtn = document.querySelector('.close-dailog');
 const hamburgerButton = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.navmenu');
 const currYearEle = document.getElementById('currentYear');
 
-// Model Show/Hide
-opnBtn.addEventListener('click', () => {
-	modal.showModal();
-	opnBtn.style.display = 'none';
-});
-
-closeBtn.addEventListener('click', () => {
-	modal.close();
-	opnBtn.style.display = 'flex';
-});
-
-// Hamburger Show/Hide
-document.addEventListener('DOMContentLoaded', function () {
-	hamburgerButton.addEventListener('click', function () {
-		navMenu.style.display = navMenu.style.display === 'flex' ? 'none' : 'flex';
-	});
-});
-
 // Setting current year in footer
 const currYear = new Date().getFullYear();
 currYearEle.innerText = currYear;
+
+// Model Show/Hide
+opnBtn.addEventListener('click', () => {
+  modal.showModal();
+  opnBtn.style.display = 'none';
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.close();
+  opnBtn.style.display = 'flex';
+});
+
+// Hamburger Show/Hide
+hamburgerButton.addEventListener('click', () => {
+  navMenu.style.display = navMenu.style.display === 'flex' ? 'none' : 'flex';
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 768) {
+    navMenu.style.display = 'flex';
+  } else if (window.innerWidth <= 768) {
+    navMenu.style.display = 'none';
+  }
+});
